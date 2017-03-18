dce0c80a8220a9a47f488195a08724114c4d7d80
Drew Engelson <drew@engelson.net>
2017-03-18 00:31:15 -0400

    Bumped version.

--- pgcryptoauth/__init__.py
+++ pgcryptoauth/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 """A Django password hasher that leverages Postgres pgcrypto encryption."""
 
-VERSION = (0, 3)
+VERSION = (0, 3, 1)
 __version__ = '.'.join([str(x) for x in VERSION])
